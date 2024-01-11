4227d1a642bcc72ad7742bb922b1c23aebe413f6
konradklodzinski <konradklodzinski@gmail.com>
2024-01-11 22:22:39 +0100

    add prototypes of functions in dvb class

--- digital_video_broadcasting/digital_video_broadcasting.hpp
+++ digital_video_broadcasting/digital_video_broadcasting.hpp
@@ -4,68 +4,86 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <stdexcept>
 #include <vector>
+#include "crc_library.hpp"
 
 namespace digital_video_broadasting
 {
-    enum class transport_stream_input_e : uint8_t
-    {
-        transport = 0b11,
-        generic_packetized = 0b00,
-        generic_continuous = 0b01,
-        reserved = 0b10
-    };
+enum class transport_stream_input_e : uint8_t
+{
+    transport          = 0b11,
+    generic_packetized = 0b00,
+    generic_continuous = 0b01,
+    reserved           = 0b10
+};
 
-    enum class single_input_stream_e : bool
-    {
-        single = 0b1,
-        multiple = 0b0
-    };
+enum class single_input_stream_e : bool
+{
+    single   = 0b1,
+    multiple = 0b0
+};
 
-    enum class constant_coding_and_modulation_e : bool
-    {
-        ccm = 0b1,
-        acm = 0b0
-    };
+enum class constant_coding_and_modulation_e : bool
+{
+    ccm = 0b1,
+    acm = 0b0
+};
 
-    enum class input_stream_synchronization_indicator_e : bool
-    {
-        active =0b1,
-        not_active = 0b0
-    };
+enum class input_stream_synchronization_indicator_e : bool
+{
+    active     = 0b1,
+    not_active = 0b0
+};
 
-    enum class null_packet_deletion_e : bool
-    {
-        active =0b1,
-        not_active = 0b0
-    };
+enum class null_packet_deletion_e : bool
+{
+    active     = 0b1,
+    not_active = 0b0
+};
 
-    enum class transmition_roll_off_factor_e :  uint8_t
-    {
-        alpha_0_35 = 0b00,
-        alpha_0_25 = 0b01,
-        alpha_0_20 = 0b10,
-        reserved = 0b11
-    };
+enum class transmition_roll_off_factor_e : uint8_t
+{
+    alpha_0_35 = 0b00,
+    alpha_0_25 = 0b01,
+    alpha_0_20 = 0b10,
+    reserved   = 0b11
+};
 
-    struct [[gnu::packed]] matype_1_s
-    {
-        transport_stream_input_e transport_stream_input :2;
-        single_input_stream_e single_input_stream_ :1;
-        constant_coding_and_modulation_e constant_coding_and_modulation :1;
-        input_stream_synchronization_indicator_e input_stream_synchronization_indicator :1;
-        null_packet_deletion_e null_packet_deletion :1;
-        transmition_roll_off_factor_e transmition_roll_off_factor :2;
-    };
-    
+struct [[gnu::packed]] matype_s
+{
+    transport_stream_input_e transport_stream_input                                 : 2;
+    single_input_stream_e single_input_stream_                                      : 1;
+    constant_coding_and_modulation_e constant_coding_and_modulation                 : 1;
+    input_stream_synchronization_indicator_e input_stream_synchronization_indicator : 1;
+    null_packet_deletion_e null_packet_deletion                                     : 1;
+    transmition_roll_off_factor_e transmition_roll_off_factor                       : 2;
+    uint8_t matype_2                                                                : 8;
+};
 
-    class dvb
-    {
-    private:
+struct base_band_header_s
+{
+    matype_s matype;
+    uint16_t user_packet_length;
+    uint16_t data_field_length;
+    uint8_t user_packet_sync;
+    uint16_t sync_distance;
+    uint8_t crc_8;
+};
 
-    public:
-        dvb(/* args */);
-    };
-}
+class dvb
+{
+  protected:
+  crc_library::crc<uint8_t, 0xD5, 0x00, false, false, 0x00> crc;
+  base_band_header_s base_band_header;
+  std::vector<std::byte> data_field;
+  public:
+    dvb(const std::vector<std::byte> &raw_data);
+    dvb(const base_band_header_s &base_band_header, const std::vector<std::byte> &data_field);
+    base_band_header_s get_base_band_header();
+    std::vector<std::byte> get_data_field();
+    std::vector<std::byte> get_raw_packet(uint packet_length);
+};
+} // namespace digital_video_broadasting
 
 #endif
